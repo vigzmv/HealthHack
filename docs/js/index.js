9d78d5cc86e2e4f9fd22f8d11c68aee9158b1f62
vigzmv <vigzmv@outlook.com>
2016-11-19 18:46:36 +0530

    selelected

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,14 +1,31 @@
+var selected = ""
+
 $(document).ready(function() {
 
-  $(".selLabel").click(function () {
-    $('.dropdown').toggleClass('active');
-  });
+    $(".selLabel").click(function() {
+        $('.dropdown').toggleClass('active');
+    });
 
-  $(".dropdown-list li").click(function() {
-    $('.selLabel').text($(this).text());
-    $('.dropdown').removeClass('active');
-    $('.selected-item p span').text($('.selLabel').text());
-    $('#select-heading').slideUp('slow');
-  });
+    $(".dropdown-list li").click(function() {
+        $('.selLabel').text($(this).text());
+        $('.dropdown').removeClass('active');
+        $('#select-heading').slideUp('slow');
+
+        selected = $('.selLabel').text().trim();
+        console.log(selected);
+        if (selected == "Liver") {
+            $('.inputform1').slideDown('slow');
+            $('.inputform2').slideUp('slow');
+            $('.inputform3').slideUp('slow');
+        } else if (selected == "Heart") {
+            $('.inputform1').slideUp('slow');
+            $('.inputform2').slideDown('slow');
+            $('.inputform3').slideUp('slow');
+        } else if (selected == "Lungs") {
+            $('.inputform1').slideUp('slow');
+            $('.inputform2').slideUp('slow');
+            $('.inputform3').slideDown('slow');
+        }
+    });
 
 });
